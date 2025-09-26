5741f7a64ea8c4b91a350f79413308005f4dfe16
RocioAV <44351449@fi.unju.edu.ar>
2025-09-26 16:30:51 -0300

    fix bug de conexion bd, cambio App.xaml.cs en app.config y en las properties de instituto.mdf build:none y do not copy

--- Vistas/App.xaml.cs
+++ Vistas/App.xaml.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Windows;
+using System.IO;
 
 namespace Vistas
 {
@@ -12,5 +13,19 @@ namespace Vistas
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Ruta base de ejecución (bin\Debug normalmente)
+            string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+
+            // Subo carpetas hasta llegar al proyecto ClasesBase\App_Data
+            // Ajustá el número de ".." según tu estructura
+            string dataDir = Path.Combine(baseDir, @"..\..\..\ClasesBase");
+
+            // Normalizo la ruta y la asigno como DataDirectory
+            AppDomain.CurrentDomain.SetData("DataDirectory", Path.GetFullPath(dataDir));
+        }
     }
 }
