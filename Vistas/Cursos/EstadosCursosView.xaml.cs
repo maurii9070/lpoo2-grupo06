d5ce21e47fc1421ae6e5eef2483124aae5c19206
SebasMullicundo <na630578@gmail.com>
2025-09-30 13:14:04 -0300

    Implementación de vista de estado de cursos y conversor de estados

---
+++ Vistas/Cursos/EstadosCursosView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vistas.Cursos
+{
+    /// <summary>
+    /// Interaction logic for EstadosCursosView.xaml
+    /// </summary>
+    public partial class EstadosCursosView : UserControl
+    {
+        public EstadosCursosView()
+        {
+            InitializeComponent();
+        }
+    }
+}
