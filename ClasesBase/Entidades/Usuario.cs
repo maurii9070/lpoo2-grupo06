2488055b0f49542600cb4241ceb8b2f5aa8ff498
Mauricio Nehuen Velazquez <mauri_i9070@hotmail.com>
2025-10-16 16:56:35 -0300

    tp 4 realizado con algunas modificaciones

--- ClasesBase/Entidades/Usuario.cs
+++ ClasesBase/Entidades/Usuario.cs
@@ -1,16 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.ComponentModel;
 
 namespace ClasesBase.Entidades
 {
-    public class Usuario
+    public class Usuario : INotifyPropertyChanged
     {
         public int Usu_ID { get; set; }
         public string Usu_NombreUsuario { get; set; }
         public string Usu_Contrasenia { get; set; }
         public string Usu_ApellidoNombre { get; set; }
         public int Rol_ID { get; set; }
+
+        // INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- ClasesBase/Repositories/UsuarioRepository.cs
+++ ClasesBase/Repositories/UsuarioRepository.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using ClasesBase.Entidades;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.ObjectModel;
+using ClasesBase.Database;
 
 namespace ClasesBase.Repositories
 {
@@ -24,5 +28,68 @@ namespace ClasesBase.Repositories
                    .FirstOrDefault(u => u.Usu_NombreUsuario == usuario &&
                                         u.Usu_Contrasenia == clave);
         }
+
+        private const string Table = "Usuario";
+
+        public ObservableCollection<Usuario> GetAll()
+        {
+            ObservableCollection<Usuario> list = new ObservableCollection<Usuario>();
+
+            string sql = "SELECT Usu_ID, Usu_NombreUsuario, Usu_Contrasenia, " +
+                         "Usu_ApellidoNombre, Rol_ID FROM " + Table +
+                         " ORDER BY Usu_ApellidoNombre, Usu_NombreUsuario";
+
+            DataTable dt = DatabaseHelper.ExecuteQuery(sql);
+
+            foreach (DataRow r in dt.Rows)
+            {
+                Usuario u = new Usuario
+                {
+                    Usu_ID = Convert.ToInt32(r["Usu_ID"]),
+                    Usu_NombreUsuario = r["Usu_NombreUsuario"].ToString(),
+                    Usu_Contrasenia = r["Usu_Contrasenia"].ToString(),
+                    Usu_ApellidoNombre = r["Usu_ApellidoNombre"].ToString(),
+                    Rol_ID = Convert.ToInt32(r["Rol_ID"])
+                };
+                list.Add(u);
+            }
+            return list;
+        }
+
+        public void Add(Usuario u)
+        {
+            string sql = "INSERT INTO " + Table + " (Usu_NombreUsuario, Usu_Contrasenia, " +
+                         "Usu_ApellidoNombre, Rol_ID) VALUES (@nom,@pass,@ape,@rol)";
+            SqlParameter[] p = {
+                new SqlParameter("@nom",  u.Usu_NombreUsuario),
+                new SqlParameter("@pass", u.Usu_Contrasenia),
+                new SqlParameter("@ape",  u.Usu_ApellidoNombre),
+                new SqlParameter("@rol",  u.Rol_ID)
+            };
+            DatabaseHelper.ExecuteNonQuery(sql, p);
+        }
+
+        public void Update(Usuario u)
+        {
+            string sql = "UPDATE " + Table + " SET Usu_NombreUsuario=@nom, " +
+                         "Usu_Contrasenia=@pass, Usu_ApellidoNombre=@ape, Rol_ID=@rol " +
+                         "WHERE Usu_ID=@id";
+            SqlParameter[] p = {
+                new SqlParameter("@nom",  u.Usu_NombreUsuario),
+                new SqlParameter("@pass", u.Usu_Contrasenia),
+                new SqlParameter("@ape",  u.Usu_ApellidoNombre),
+                new SqlParameter("@rol",  u.Rol_ID),
+                new SqlParameter("@id",   u.Usu_ID)
+            };
+            DatabaseHelper.ExecuteNonQuery(sql, p);
+        }
+
+        public void Delete(int id)
+        {
+            string sql = "DELETE FROM " + Table + " WHERE Usu_ID=@id";
+            SqlParameter[] p = { new SqlParameter("@id", id) };
+            DatabaseHelper.ExecuteNonQuery(sql, p);
+        }
     }
+    
 }
